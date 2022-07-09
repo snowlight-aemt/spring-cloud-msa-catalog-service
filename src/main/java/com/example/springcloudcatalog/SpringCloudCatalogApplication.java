757362afe02f6db8bd8597bfaaf32abafc7ee5a6
private desktop pc <it.s.aemt@gmail.com>
2022-07-09 16:13:41 +0900

    EnableEurekaClient

--- src/main/java/com/example/springcloudcatalog/SpringCloudCatalogApplication.java
+++ src/main/java/com/example/springcloudcatalog/SpringCloudCatalogApplication.java
@@ -3,10 +3,12 @@ package com.example.springcloudcatalog;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.ui.ModelMap;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudCatalogApplication {
 
 	public static void main(String[] args) {
